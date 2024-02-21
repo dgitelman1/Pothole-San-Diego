ecedca492856ac4dd96c00321b3b67f0eebfde7e
Daniel G <92356476+dgitelman1@users.noreply.github.com>
2024-02-21 13:36:45 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let gdata = [];
 let potholes = [];
 let pothole_count = [];
+let valid_zip = [];
 
 let projection = d3.geoMercator();
 let path = d3.geoPath().projection(projection);
 
-
 const to_remove = ['91901', '91905', '91906', '92028', '92036', '92058', '92058', '91916', '91934', '91935', '91917', '91931', '91948', '91962', '92058', '92058', '92058', '91963', '91980', '92003', '92027', '92004', '92019', '92021', '92026', '92040', '92054', '92055', '92056', '92057', '92058', '92058', '92058', '92059', '92060', '92061', '92065', '92083', '92083', '92084', '92086', '92066', '92069', '92070', '92081', '92082', '92259', '92536', '92672'];
+
 
 // Function to load JSON data
 async function loadGeoData() {
@@ -20,6 +21,7 @@ async function loadGeoData() {
         const data = await response_g.json();
         gdata = data;
         gdata.features = gdata.features.filter((d) => !to_remove.includes(d.properties.zip));
+        valid_zip = gdata.features.map((d) => d.properties.zip);
         draw_map();
         
     } catch (error) {
@@ -40,8 +42,17 @@ async function loadData() {
     }
 }
 
+async function full_load(){
+    await loadData();
+    await loadGeoData();
+    document.getElementById("loading-image").style.display="none";
+}
+
+// Map display functions
+
 function draw_map() {
     const svg = d3.select('#map');
+    svg.attr('display', 'block')
     const width = svg.attr('width');
     const height = svg.attr('height');
     projection.fitSize([width, height], gdata);
@@ -82,80 +93,24 @@ function draw_map() {
     .attr('id', function (d) {return d.properties.zip;})
     .style('stroke', 'black');
 
-    const scale = d3.scaleLinear([density(0), density(1)], ['grey', 'red']);
+    let dens_extent = density();
+    const scale = d3.scaleLinear([dens_extent[0], dens_extent[1]], ['grey', 'red']);
 
     svg.selectAll('path').style(
       'fill',
       function (d) {
-        let zip_count = pothole_count.get(d.properties.zip);
-        if (zip_count == undefined) {
-          zip_count = 0;
-        }
+        let zip_count = z_count(d.properties.zip)
         return scale(zip_count / this.getTotalLength());
       }
     );
     generate_potholes(.1, potholes, svg, 'black');
   }
 
-function clicked(event, d) {
-    let z = d.properties.zip;
-    view = d.properties.community;
-    current_zip = z;
-    p_c = z_count(z);
-    display_segment(z);
-}
-
-function revert(event, d) {
-    const svg = d3.select('#map');
-    svg.selectAll('path').remove();
-    svg.selectAll('circle').remove();
-    draw_map();
-}
-
-function density(m) {
-    const svg = d3.select('#map');
-    if (m == 1) {
-        return d3.max(svg.selectAll('path')._groups[0], function (d) {
-        let zip_count = z_count(d.__data__.properties.zip);
-        return zip_count / d.getTotalLength();
-        });
-    } else {
-        return d3.min(svg.selectAll('path')._groups[0], function (d) {
-        let zip_count = z_count(d.__data__.properties.zip);
-        return zip_count / d.getTotalLength();
-        });
-    }
-}
-
-
-function z_count(zip) {
-    let zip_count = pothole_count.get(zip);
-    if (zip_count == undefined) {
-      return 0;
-    } else {
-      return zip_count;
-    }
-  }
-
-function generate_potholes(radius, pdata, svg, color) {
-    svg.selectAll('circle')
-    .data(pdata)
-    .enter()
-    .append('circle')
-    .attr('cx', (d) => projection([d.lng, d.lat])[0])
-    .attr('cy', (d) => projection([d.lng, d.lat])[1])
-    .attr('r', radius)
-    .style('fill', color);
-}
-
-
-function search_zipcode() {
-    let input = document.getElementById('search').value
-    console.log(input)
-  }
-
-function display_segment(z){
-    const svg = d3.select('#map');
+  function display_segment(z){
+    const svg1 = d3.select('#map');
+    svg1.attr('display', "none")
+    const svg = d3.select('#map1');
+    svg.attr('display', "block")
     const width = svg.attr('width');
     const height = svg.attr('height');
     d3.select('#tooltip')
@@ -181,11 +136,71 @@ function display_segment(z){
     generate_potholes(1.5, d3.group(potholes, (d) => d.zipcode).get(z), svg, 'red');
 }
 
+// Event functions
 
+// Displays new segment on click
+function clicked(event, d) {
+    let z = d.properties.zip
+    display_segment(z);
+}
+
+// Reverts to full map on click
+function revert(event, d) {
+    const svg = d3.select('#map1');
+    svg.attr('display', "none")
+    draw_map();
+}
+
+// Utility functions
+
+// Returns the density of potholes in an area of the map for color scale
+function density() {
+    const svg = d3.select('#map');
+    return d3.extent(svg.selectAll('path')._groups[0], function (d) {
+        let zip_count = z_count(d.__data__.properties.zip);
+        return zip_count / d.getTotalLength();
+        });
+}
+
+// Returns the count of potholes in a given zipcode
+function z_count(zip) {
+    let zip_count = pothole_count.get(zip);
+    if (zip_count == undefined) {
+      return 0;
+    } else {
+      return zip_count;
+    }
+  }
+
+// Generates potholes for the given args
+function generate_potholes(radius, pdata, svg, color) {
+    svg.selectAll('circle')
+    .data(pdata)
+    .enter()
+    .append('circle')
+    .attr('cx', (d) => projection([d.lng, d.lat])[0])
+    .attr('cy', (d) => projection([d.lng, d.lat])[1])
+    .attr('r', radius)
+    .style('fill', color);
+}
+
+function zipEventResponse(){
+    document.getElementById('sbutton').addEventListener('click', function() {
+        let input = document.getElementById('search').value
+        if (!valid_zip.includes(input)){
+            console.log(error)
+        }else{
+            display_segment(input)
+        }
+    })
+    document.getElementById('rbutton').addEventListener('click', function() {
+        revert()
+    })
+}
 
 // Load data when the page loads
 window.addEventListener('DOMContentLoaded', (event) => {
-    loadData();
-    loadGeoData();
+    full_load()
+    zipEventResponse()
+    document.getElementById("maps").style.margin = "50px 10px 20px 30px";
 });
-
